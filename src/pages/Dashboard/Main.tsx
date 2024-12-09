2e2990f771f266ed36a40efaf672a8ea2bee8e8c
mardon1dev <dusbekovmardonbek@gmail.com>
2024-12-09 15:09:00 +0500

    Dashboard is added

---
+++ src/pages/Dashboard/Main.tsx
@@ -0,0 +1,5 @@
+const Main = () => {
+  return <div>Dashboard</div>;
+};
+
+export default Main;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,21 @@
-import { Route, Routes } from "react-router-dom"
-import { LoginPage, RegisterPage } from "../pages"
+import { Route, Routes } from "react-router-dom";
+import { LoginPage, RegisterPage } from "../pages";
+import DashboardWrapper from "../pages/Dashboard/DashboardWrapper";
+import { Brands, Categories, Main, Products } from "../pages/Dashboard";
 
 const CustomRoutes = () => {
   return (
     <Routes>
-      <Route path="/login" element={<LoginPage/>}/>
-      <Route path="/register" element={<RegisterPage/>}/>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/" element={<DashboardWrapper />}>
+        <Route path="/" element={<Main />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/brands" element={<Brands />} />
+        <Route path="/categories" element={<Categories />} />
+      </Route>
     </Routes>
-  )
-}
+  );
+};
 
-export default CustomRoutes
+export default CustomRoutes;
