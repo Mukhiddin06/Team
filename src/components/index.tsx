7e5297b6a78ae2afc799b7a0cd4936e56b7f4088
Oyatillo12 <fayzillo998800@gmail.com>
2024-12-09 15:09:32 +0500

    added login page

---
+++ src/components/index.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+
+interface ProtectedRouteProps {
+  children: ReactNode;
+}
+
+const ProtectedLayout = ({ children }:ProtectedRouteProps) => {
+  const isAuthenticated = !!localStorage.getItem('access_token'); 
+  const location = useLocation(); 
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return <>{children}</>;
+};
+
+const ProtectedAuth = ({ children }:ProtectedRouteProps) => {
+    const isAuthenticated = !!localStorage.getItem('access_token');
+    if (isAuthenticated) {
+      return <Navigate to="/" replace />;
+    }
+  
+    return <>{children}</>;
+  };
+export {ProtectedLayout,ProtectedAuth};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,28 @@
-import { Route, Routes } from "react-router-dom"
-import { LoginPage, RegisterPage } from "../pages"
+import { Route, Routes } from "react-router-dom";
+import { LoginPage, RegisterPage } from "../pages";
+import { ProtectedAuth } from "../components";
 
 const CustomRoutes = () => {
   return (
     <Routes>
-      <Route path="/login" element={<LoginPage/>}/>
-      <Route path="/register" element={<RegisterPage/>}/>
+      <Route
+        path="/login"
+        element={
+          <ProtectedAuth>
+            <LoginPage />
+          </ProtectedAuth>
+        }
+      />
+      <Route
+        path="/register"
+        element={
+          <ProtectedAuth>
+            <RegisterPage />
+          </ProtectedAuth>
+        }
+      />
     </Routes>
-  )
-}
+  );
+};
 
-export default CustomRoutes
+export default CustomRoutes;
